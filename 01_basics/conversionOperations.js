7debbc6ffb3526cb762c3719d6e8d03633772400
Sufiyan Rafique <maliksufuan7@gmail.com>
2025-07-22 07:45:49 +0000

    operators

--- 01_basics/conversionOperations.js
+++ 01_basics/conversionOperations.js
@@ -27,3 +27,46 @@ let stringNumber = String(someNumber);
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
 
+// *******************  Operations  ************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str = "Hello"
+let str2 = " Sufiyan"
+
+let str3 = str + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+// console.log(+true);
+// console.log(-true);
+// console.log(+"");
+
+let num1, num2, num3 
+num1 = num2 = num3 = 2 + 2
+
+// console.log(num1);
+
+let gameCounter = 100
+gameCounter++
+// console.log(gameCounter)
+
+let gameCounter2 = 100
+++gameCounter2
+// console.log(gameCounter2)
+

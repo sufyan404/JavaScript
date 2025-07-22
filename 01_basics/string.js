fe56fb7e7d08121c47be4d848c108a8ae67e29be
Sufiyan Rafique <maliksufuan7@gmail.com>
2025-07-22 12:16:17 +0000

    string

---
+++ 01_basics/string.js
@@ -0,0 +1,38 @@
+const name = "Sufiyan-Agri-Tech";
+const age = 25;
+
+// console.log(name + " age " + age);
+// console.log(`Hello my name is ${name} and I am ${age} years old.`);
+// console.log(name.split('-'));
+
+
+const gameName = new String("Call of Duty");
+// console.log(gameName);
+// console.log(gameName[0]);
+// console.log(gameName.length);
+// console.log(gameName.__proto__);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf("t"));
+
+const newStr = gameName.substring(0, 6)
+// console.log(newStr);
+
+const anotherStr = gameName.slice(-11, 4)
+// console.log(anotherStr);
+
+const newStr2 = "         Sufiyan           "
+// console.log(newStr2);
+// console.log(newStr2.trim());
+// console.log(newStr2.trimStart());
+// console.log(newStr2.trimEnd());
+
+
+const url = "https://www.sufiagroz.com/sufi%20agroz"
+// url.replace(/%20/, '-')
+
+// console.log(url.replace(/%20/, '-'));
+// console.log(url.includes('sufiagroz'));
+
+
+

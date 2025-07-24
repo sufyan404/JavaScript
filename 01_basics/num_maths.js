f6e4aaf5edf5b9ee9ba60218b3a296a6d3f4967d
Sufiyan Rafique <maliksufuan7@gmail.com>
2025-07-24 08:46:00 +0000

    math

---
+++ 01_basics/num_maths.js
@@ -0,0 +1,45 @@
+let score = 200;
+// console.log(score);
+
+let runs = new Number(200.23232)
+// console.log(runs);
+
+// console.log(runs.toString());
+// console.log(runs.toString().length);
+// console.log(runs.toFixed(2));
+
+const num = 23.245454
+// console.log(num.toPrecision(2));
+// console.log(num.toPrecision(3));
+
+
+const hundreds = 100000000000
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+// ++++++++++++++++  Maths   +++++++++++++++
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.7));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(3, 5, 6, 0));
+
+
+// console.log(Math.random());
+// console.log(Math.random()*9 + 1);
+
+// console.log((Math.random()*9) + 1);
+// console.log(Math.floor(Math.random()*9) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.round(Math.random() * (max - min + 1)) + min)
+
+
+

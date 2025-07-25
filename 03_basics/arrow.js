4294ac1369fcdc2862beb0f819734bd0a807a115
Sufiyan Rafique <maliksufuan7@gmail.com>
2025-07-25 10:40:18 +0000

    this keyword in arrow function

--- 03_basics/arrow.js
+++ 03_basics/arrow.js
@@ -3,6 +3,34 @@ const user = {
     price: 999,
 
     welcomeMessage: function(){
-     
+    //  console.log(`${this.username}: Welcome to Website`);
+    // console.log(this);
+    
     }
 }
+user.welcomeMessage()
+user.username = "Ali"
+user.welcomeMessage()
+
+// console.log(this);
+
+
+
+function func(){
+    let username = "summy"
+    // console.log(this.username);  //   "this" WORKS ONLY IN OBJECT
+    // console.log(this);       //   WORKS
+    
+}
+func()
+
+
+const chai = () => {
+    let username = "summy"
+    // console.log(this.username);
+    // console.log(this);  //     DOES NOT WORK IN ARROW
+    
+}
+chai()
+
+

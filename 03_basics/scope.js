7fe6ec54876329002881ab145120fccdcbd075d3
Sufiyan Rafique <maliksufuan7@gmail.com>
2025-07-25 09:38:39 +0000

    scope

---
+++ 03_basics/scope.js
@@ -0,0 +1,15 @@
+var c = 40
+let a = 50
+if(true){
+    let a = 10
+    const b = 20
+    // var c = 30
+    console.log('inner ',a);
+    
+}
+
+// console.log(a);
+// console.log(b);
+console.log(c);
+
+

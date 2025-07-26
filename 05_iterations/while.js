23493e62068056fa3f0bc1ee95ae3b70ad3dc410
Sufiyan Rafique <maliksufuan7@gmail.com>
2025-07-26 11:04:44 +0000

    while loop

---
+++ 05_iterations/while.js
@@ -0,0 +1,16 @@
+
+let i = 0
+while (i <= 5) {
+    // console.log(`value of index is    ${i}`);
+    i = i + 1
+}
+
+
+let arr = ['aaa', 'bbb', 'ccc']
+let index = 0
+while (index < arr.length) {
+    // console.log(`hero is ${arr[index]}`);
+    index += 1
+}
+
+

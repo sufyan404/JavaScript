f46d829495984e9f3fee406636a537691844e46e
Sufiyan Rafique <maliksufuan7@gmail.com>
2025-07-26 07:31:12 +0000

    control

--- control_flow/switch.js
+++ control_flow/switch.js
@@ -8,19 +8,41 @@
 // }
 
 
-const month = 3
+// const month = 3
+
+// switch (month) {
+//     case 1:
+//         console.log("jan");
+//         break;
+//     case 2:
+//         console.log("feb");
+//         break;
+//     case 3:
+//         console.log("march");
+//         break;
+//     case 4:
+//         console.log("april");
+//         break;
+
+//     default:
+//         console.log('nothing matched');
+//         break;
+// }
+
+
+const month = "march"
 
 switch (month) {
-    case 1:
+    case "jan":
         console.log("jan");
         break;
-    case 2:
+    case "feb":
         console.log("feb");
         break;
-    case 3:
+    case "march":
         console.log("march");
         break;
-    case 4:
+    case "april":
         console.log("april");
         break;
 

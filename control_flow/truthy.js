deeb53890944498197063a73471b82e481274434
Sufiyan Rafique <maliksufuan7@gmail.com>
2025-07-26 07:42:43 +0000

    truthy falsey

---
+++ control_flow/truthy.js
@@ -0,0 +1,35 @@
+// const email = 'some@google.ai'
+
+// if (email) {
+//     console.log('got email')
+// } else {
+//     console.log('no email');
+// }
+
+
+// const email = ''
+
+// if (email) {
+//     console.log('got email')
+// } else {
+//     console.log('no email');
+// }
+
+
+const email = []
+const data = {}
+
+if (email) {
+    // console.log('got email')
+} else {
+    // console.log('no email');
+}
+
+
+//  FALSY VALUES
+//  false, 0, "", -0, bigint 0n, null, undefined, NAN
+
+//  TRUTHY VALUE
+//  ture, "0", "false", " ", [], {}, function(){}
+
+

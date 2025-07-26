edd19ee91b954de9c0cd2dd9a8e597c1ead94d68
Sufiyan Rafique <maliksufuan7@gmail.com>
2025-07-26 10:14:20 +0000

    break and continue

--- 05_iterations/loops.js
+++ 05_iterations/loops.js
@@ -28,3 +28,21 @@
 
 
 
+// break and continue
+
+// for (let i = 1; i <= 10; i++) {
+    //     if (i === 5) {
+        //         console.log(`detected 5`);
+        //         break
+        //     }
+        //     console.log(`value of i is: ${i}`);
+// }
+
+
+for (let i = 1; i <= 10; i++) {
+    if (i === 5) {
+        console.log(`detected 5`);
+        continue
+    }
+    console.log(`value of i is: ${i}`);
+}

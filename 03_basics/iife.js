c79b81599489a9d56887929effa7dfd1a287586d
Sufiyan Rafique <maliksufuan7@gmail.com>
2025-07-26 06:00:06 +0000

    iife

---
+++ 03_basics/iife.js
@@ -0,0 +1,24 @@
+(function one (){
+    // console.log('db connected');
+})();
+
+
+// ( () => {
+//     console.log('connected');
+    
+// })()   //  needs semicolon in last iife
+
+
+// ( function one() {
+//     console.log('connected');
+    
+// })()  // semicolon
+
+
+( (backend) => {
+    // console.log(`connected ${backend}`);
+    
+})('node')  
+
+
+

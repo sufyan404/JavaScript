2c7178494f827aa0645e0125790468e8cbbea4c9
Sufiyan Rafique <maliksufuan7@gmail.com>
2025-07-26 07:54:56 +0000

    terniary operator

--- control_flow/truthy.js
+++ control_flow/truthy.js
@@ -50,4 +50,13 @@ let val1;
 // val1 = undefined ?? 10
 val1 = null ?? 10 ?? 20
 
-console.log(val1);
+// console.log(val1);
+
+
+//   Terniary Operator 
+
+// condition ? ture : false
+
+const price = 100
+price <= 80 ? console.log('less than 80') : console.log("more than 80");
+

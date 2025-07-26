7da44da0bab15f0b93afb4a2d76f6f7461b1de69
Sufiyan Rafique <maliksufuan7@gmail.com>
2025-07-26 10:07:46 +0000

    for loop

---
+++ 05_iterations/loops.js
@@ -0,0 +1,30 @@
+// for (let i = 1; i <= 10; i++) {
+//     const element = i;
+// if(element === 5){
+//     // console.log('next 5');
+    
+// }
+//     // console.log(element);
+// }
+
+
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`outer loop: ${i}`);
+//     for (let k = 1; k <= 10; k++) {
+//         // console.log(`inner: ${k} and outer ${i}`);
+//         console.log(i + " x " + k + "   =   " + i*k);
+//     }
+// }
+
+
+// let arr = ['aaa', 'bbb', 'ccc', 'ddd']
+// console.log(arr.length);
+
+// for (let i = 0; i < arr.length; i++) {
+//     const element = arr[i];
+//     console.log(element);   
+// }
+
+
+

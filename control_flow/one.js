12923c476ca67df533a6425c54b6a08734cd5c07
Sufiyan Rafique <maliksufuan7@gmail.com>
2025-07-26 07:09:52 +0000

    if else

---
+++ control_flow/one.js
@@ -0,0 +1,33 @@
+//  if
+
+const isLoggedIn = true
+const temp = 42
+
+if (isLoggedIn) {
+    
+}
+
+if (temp < 50){
+    // console.log('less than 50');
+}
+else{
+    // console.log('grater than 50');
+}
+
+
+if (2 == "2") {
+    // console.log('executed');
+}
+
+//  ++++++++++   COMPARISON    +++++++++++
+//    >, <, =>, <=, ==, ===, !=, !==
+
+
+const score = 100
+
+if (score > 50) {
+    const power = 'fly'
+    // console.log(`use power: ${power}`);
+}
+
+

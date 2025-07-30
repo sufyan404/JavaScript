943b6a486351af235d52a0f5014fa5f1587fb92e
Sufiyan Rafique <maliksufuan7@gmail.com>
2025-07-30 04:04:20 +0000

    map

--- 05_iterations/high_order.js
+++ 05_iterations/high_order.js
@@ -19,3 +19,27 @@ for (const greetings of srt) {
 
 // Maps
 
+const map = new Map()
+map.set('PK', 'Pakistan')
+map.set('USA', 'America')
+map.set('fr', 'Francs')
+map.set('fr', 'Francs')
+
+// console.log(map);
+
+for (const items of map) {
+    // console.log(items);
+}
+
+for (const [items, value] of map) {
+    // console.log(items, value);
+}
+
+const myObj = {
+    'game1': 'abc',
+    'game2': 'xyz'
+}
+
+for (const [key, value] of object) {
+    // console.log(key, value);   // NOT ITERATABLE
+}

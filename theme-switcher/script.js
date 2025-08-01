7648ef2ad45946d97ed2aec770aebdfccf6ed0e1
Sufiyan Rafique <maliksufuan7@gmail.com>
2025-08-01 10:46:27 +0000

    project one

---
+++ theme-switcher/script.js
@@ -0,0 +1,8 @@
+const buttons = document.querySelectorAll('.button')
+const body = document.querySelector('body')
+// console.log(buttons);
+
+buttons.forEach((button)=>{
+    console.log(button);
+})
+

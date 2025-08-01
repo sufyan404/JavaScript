7641a72290802e6b3cedd7dd0eada4a16e300615
Sufiyan Rafique <maliksufuan7@gmail.com>
2025-08-01 12:46:18 +0000

    first complete

--- theme-switcher/script.js
+++ theme-switcher/script.js
@@ -3,6 +3,19 @@ const body = document.querySelector('body')
 // console.log(buttons);
 
 buttons.forEach((button)=>{
-    console.log(button);
+    button.addEventListener('click', (event)=>{
+         if (event.target.id === 'green') {
+            body.style.backgroundColor = event.target.id
+         }
+         if (event.target.id === 'white') {
+            body.style.backgroundColor = event.target.id
+         }
+         if (event.target.id === 'blue') {
+            body.style.backgroundColor = event.target.id
+         }
+         if (event.target.id === 'yellow') {
+            body.style.backgroundColor = event.target.id
+         }
+    })
 })
 
